d30d2b16a181375d7daf25bc5794603834600fd2
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-05-11 23:03:40 +0700

    video ke-6 done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman About</title>
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-
-    <h1>Halaman About</h1>
-
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman About</h3>
+    <p>Nama Saya {{ $nama }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,63 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About', 'nama' => 'Naufal Harits']);
 });
 
-Route::get('/blog', function () {
-    return view('about');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Naufal Harits',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odit adipisci,
+            obcaecati, enim, blanditiis ssumenda quam nihil aut error minus sunt. Iusto, illo fugit e'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Prasetia harits',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odit adipisci,
+            obcaecati, enim, blanditiis ssumenda quam nihil aut error minus sunt. Iusto, illo fugit e'
+        ]
+    ]]);
+});
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Naufal Harits',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odit adipisci,
+            obcaecati, enim, blanditiis ssumenda quam nihil aut error minus sunt. Iusto, illo fugit e'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Prasetia harits',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odit adipisci,
+            obcaecati, enim, blanditiis ssumenda quam nihil aut error minus sunt. Iusto, illo fugit e'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => ' Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
